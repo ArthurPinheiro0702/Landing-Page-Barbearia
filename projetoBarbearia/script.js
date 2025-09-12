994ad3090c2eca3c05c65c63915f1bc11751672a
ArthurPinheiro <arthur.pinheiro@cerfox.com.br>
2025-09-12 09:32:21 -0300

    responsividade

--- projetoBarbearia/script.js
+++ projetoBarbearia/script.js
@@ -1,11 +1,11 @@
-/* Quando o usuário scrolar para baixo, a página ativa o blur no navbar */
-window.addEventListener("scroll", function() {
-        const navbar = document.querySelector(".navbar");
-        if (window.scrollY > 0) {
-            navbar.classList.add("scrolled");
-            console.log("scrolled");
-        } else {
-            navbar.classList.remove("scrolled");
-        }
-});
+const mensagemPrincipal = document.getElementById('mensagem-centro');
+const imagemTelasMenores = document.getElementById('imagemTelasMenores');
 
+function atualizarMensagem() {
+    const larguraTela = window.innerWidth;
+    if (larguraTela < 990) {
+        imagemTelasMenores.style.backgroundImage = "url('img/fundoResponsivo.png')";
+    } else {
+        imagemTelasMenores.style.backgroundImage = ""; // Remove or reset for larger screens
+    }
+}
