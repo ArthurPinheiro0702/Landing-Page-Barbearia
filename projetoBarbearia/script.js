daf17cb726b98230e68b557bcfe5acdf68fa2064
ArthurPinheiro0702 <arthurdallagnol0702@gmail.com>
2025-09-13 11:39:00 -0300

    amor da julia

--- projetoBarbearia/script.js
+++ projetoBarbearia/script.js
@@ -6,6 +6,6 @@ function atualizarMensagem() {
     if (larguraTela < 990) {
         imagemTelasMenores.style.backgroundImage = "url('img/fundoResponsivo.png')";
     } else {
-        imagemTelasMenores.style.backgroundImage = ""; // Remove or reset for larger screens
+        imagemTelasMenores.style.backgroundImage = "";
     }
 }
